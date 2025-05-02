d572760a4638a0f526e1d1e4a7087ef492237039
Ivan Belokobylskiy <belokobylskij@gmail.com>
2025-05-02 11:14:38 +0400

    try 3

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -81,15 +81,15 @@ MODULE_PARM_DESC(rtw_ips_mode, "The default IPS mode");
 module_param(rtw_lps_level, int, 0644);
 MODULE_PARM_DESC(rtw_lps_level, "The default LPS level");
 
-/* LPS: 
+/* LPS:
  * rtw_smart_ps = 0 => TX: pwr bit = 1, RX: PS_Poll
  * rtw_smart_ps = 1 => TX: pwr bit = 0, RX: PS_Poll
  * rtw_smart_ps = 2 => TX: pwr bit = 0, RX: NullData with pwr bit = 0
 */
 int rtw_smart_ps = 2;
 
-#ifdef CONFIG_WMMPS_STA	
-/* WMMPS: 
+#ifdef CONFIG_WMMPS_STA
+/* WMMPS:
  * rtw_smart_ps = 0 => Only for fw test
  * rtw_smart_ps = 1 => Refer to Beacon's TIM Bitmap
  * rtw_smart_ps = 2 => Don't refer to Beacon's TIM Bitmap
@@ -1649,6 +1649,7 @@ void rtw_os_ndev_unregister(_adapter *adapter)
 
 	netdev = adapter->pnetdev;
 
+#if defined(RTW_SINGLE_WIPHY)
 #if defined(CONFIG_IOCTL_CFG80211)
 	rtw_cfg80211_ndev_res_unregister(adapter);
 #endif
@@ -1669,6 +1670,7 @@ void rtw_os_ndev_unregister(_adapter *adapter)
 #endif
 	rtw_wiphy_unregister(adapter_to_wiphy(adapter));
 #endif
+#endif /* defined(RTW_SINGLE_WIPHY) */
 
 #ifdef CONFIG_RTW_NAPI
 	if (adapter->napi_state == NAPI_ENABLE) {
